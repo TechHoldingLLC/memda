ed38b9db6b4b50d9a4bd18c2733ecc6bdc771f80
Yorick <yorick@ctrlalt.dev>
2020-11-17 22:13:16 -0800

    feat: add tests

---
+++ src/main_test.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"os"
+	"testing"
+)
+
+// TestParseArgs Test parseArgs function
+func TestParseArgs(t *testing.T) {
+	os.Args = append(os.Args, "--profile=test")
+	os.Args = append(os.Args, "--region=us-west-2")
+	os.Args = append(os.Args, `--lambda=test`)
+	os.Args = append(os.Args, "--limit=20")
+
+	profile, region, lambda, limit := parseArgs()
+
+	if profile != "test" {
+		t.Fail()
+	}
+	if region != "us-west-2" {
+		t.Fail()
+	}
+	if lambda != "test" {
+		t.Fail()
+	}
+	if limit != 20 {
+		t.Fail()
+	}
+}
